1a6272b0ecf7648abecea00023eedf5591d60f68
Sonle <103118588+sonle1501@users.noreply.github.com>
2026-05-06 20:04:20 +0700

    fix: @Param in UserListDetailsRepository. Detailed reason below
    In older versions of the framework, Spring magically extract the names of parameters (userId, listId, itemId). New spring is aggressively optimized startup times and resource usage -> Spring relies strictly on standard Java reflection , so we must annotate these fields

--- modules/users/src/main/java/dev/sonle/filmdb/users/repository/UserListDetailsRepository.java
+++ modules/users/src/main/java/dev/sonle/filmdb/users/repository/UserListDetailsRepository.java
@@ -3,6 +3,7 @@ package dev.sonle.filmdb.users.repository;
 import dev.sonle.filmdb.users.model.UserListDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -17,7 +18,9 @@ public interface UserListDetailsRepository extends JpaRepository<UserListDetails
           AND ud.listId = :listId 
           AND ul.userId = :userId
     """)
-    boolean existByUserIdAndUserListIdAndItemId(UUID userId, UUID listId, UUID itemId);
-
-
+    boolean existByUserIdAndUserListIdAndItemId(
+            @Param("userId") UUID userId,   // must use @Param for specific in new spring
+            @Param("listId") UUID listId,
+            @Param("itemId") UUID itemId
+    );
 }
