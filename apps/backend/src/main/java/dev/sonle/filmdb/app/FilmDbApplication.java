8b1eee3ef3e811063bf1fae917de8ac2f3ad4f32
Trung Sơn Lê <103118588+sonle1501@users.noreply.github.com>
2026-05-28 06:56:17 +0700

    feat: prepare for TMDB image feature
    idea: using TMDB's aps to display movie's image in frontend (IMDB dataset more data-oriented)

--- apps/backend/src/main/java/dev/sonle/filmdb/app/FilmDbApplication.java
+++ apps/backend/src/main/java/dev/sonle/filmdb/app/FilmDbApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = "dev.sonle.filmdb") // detect @Repository bean
 @EntityScan(basePackages = "dev.sonle.filmdb")                // detect @Entity bean
 @org.springframework.scheduling.annotation.EnableAsync
+@org.springframework.cache.annotation.EnableCaching
 public class FilmDbApplication {
     public static void main(String[] args) {
 //        System.setProperty("spring.output.ansi.enabled", "ALWAYS");
