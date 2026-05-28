d89b21887498665adde5e6de6d103737476a1d82
Trung Sơn Lê <103118588+sonle1501@users.noreply.github.com>
2026-05-28 20:51:39 +0700

    feat: modify the http status, increase the meaning

--- modules/shared/src/main/java/dev/sonle/filmdb/shared/exception/BusinessExceptionCode.java
+++ modules/shared/src/main/java/dev/sonle/filmdb/shared/exception/BusinessExceptionCode.java
@@ -6,20 +6,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum BusinessExceptionCode {
     // Business & Domain Errors (404, 400, 409)
-    MOVIE_NOT_FOUND("IMDB domain", "Movie not found in the database", HttpStatus.NOT_FOUND),
-    MOVIE_INFO_NOT_FOUND("IMDB domain", "Movie extra info not found in the database", HttpStatus.NOT_FOUND),
-    PERSON_NOT_FOUND("IMDB domain", "Person info not found in the database", HttpStatus.NOT_FOUND),
-    USER_ALREADY_EXISTS("USER domain" ,"USER_ALREADY_EXISTS", HttpStatus.CONFLICT),
-    LIST_ALREADY_EXISTS("USER domain" ,"LIST_ALREADY_EXISTS", HttpStatus.CONFLICT),
-    ITEM_ALREADY_EXISTS("USER domain" ,"ITEM ALREADY EXIST", HttpStatus.CONFLICT),
-    USER_NOT_FOUND("USER domain" ,"USER_NOT_FOUND", HttpStatus.NOT_FOUND),
-    LIST_NOT_FOUND("USER domain" ,"LIST_NOT_FOUND", HttpStatus.NOT_FOUND),
-    ITEM_NOT_FOUND("USER domain" ,"LIST_ITEM_NOT_FOUND", HttpStatus.NOT_FOUND),
-    ITEM_NOT_VALID("USER domain" ,"LIST_ITEM_NOT_VALID", HttpStatus.NOT_FOUND),
-    INVALID_INPUT("USER domain", "INVALID_INPUT", HttpStatus.CONFLICT),
-    UNAUTHORIZED_ACCESS("USER domain", "Access denied", HttpStatus.FORBIDDEN),
-    REJECT_REQUEST("ADMIN domain", "The request is rejected", HttpStatus.FORBIDDEN),
-    RESOURCE_NOT_FOUND("ADMIN domain", "Cannot found resource", HttpStatus.NOT_FOUND);
+    MOVIE_NOT_FOUND("IMDB domain", "MOVIE_NOT_FOUND", HttpStatus.NOT_FOUND),
+    MOVIE_INFO_NOT_FOUND("IMDB domain", "MOVIE_INFO_NOT_FOUND", HttpStatus.NOT_FOUND),
+    PERSON_NOT_FOUND("IMDB domain", "PERSON_NOT_FOUND", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS("USER domain", "USER_ALREADY_EXISTS", HttpStatus.CONFLICT),
+    LIST_ALREADY_EXISTS("USER domain", "LIST_ALREADY_EXISTS", HttpStatus.CONFLICT),
+    ITEM_ALREADY_EXISTS("USER domain", "ITEM_ALREADY_EXISTS", HttpStatus.CONFLICT),
+    USER_NOT_FOUND("USER domain", "USER_NOT_FOUND", HttpStatus.NOT_FOUND),
+    LIST_NOT_FOUND("USER domain", "LIST_NOT_FOUND", HttpStatus.NOT_FOUND),
+    ITEM_NOT_FOUND("USER domain", "ITEM_NOT_FOUND", HttpStatus.NOT_FOUND),
+    ITEM_NOT_VALID("USER domain", "ITEM_NOT_VALID", HttpStatus.BAD_REQUEST),
+    INVALID_INPUT("USER domain", "INVALID_INPUT", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED_ACCESS("USER domain", "UNAUTHORIZED_ACCESS", HttpStatus.FORBIDDEN),
+    REJECT_REQUEST("ADMIN domain", "REJECT_REQUEST", HttpStatus.FORBIDDEN),
+    RESOURCE_NOT_FOUND("ADMIN domain", "RESOURCE_NOT_FOUND", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String defaultMsg;
