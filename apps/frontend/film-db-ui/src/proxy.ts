9b89521f9e723e2fc01fe062d7ebd4c07dece153
Trung Sơn Lê <103118588+sonle1501@users.noreply.github.com>
2026-05-28 16:41:31 +0700

    refactor: nextjs frontend, using proxy instaed of middleware

--- apps/frontend/film-db-ui/src/proxy.ts
+++ apps/frontend/film-db-ui/src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // Add auth protection logic here
   // const token = request.cookies.get('token')
   // if (!token && request.nextUrl.pathname.startsWith('/profile')) {
