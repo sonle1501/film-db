8b6e9b95c42dfc5dc83e886fa6a06781b1cda214
Trung Sơn Lê <103118588+sonle1501@users.noreply.github.com>
2026-06-06 16:38:30 +0700

    feat: setup docker

--- apps/frontend/film-db-ui/next.config.ts
+++ apps/frontend/film-db-ui/next.config.ts
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   rewrites: () => {
+    const backendUrl = process.env.INTERNAL_API_URL || 'http://localhost:8080';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
